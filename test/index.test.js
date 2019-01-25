2b64dce4fa4519f5251f18895be13008b902f555
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-01-25 13:58:25 +0000

    2: sum numbers separated by commads

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,20 @@
 // String Calculatr Kata
 
+function addNumbers(expression) {
+    let expr_splited = expression.split(',')
+    return expr_splited.reduce(add, 0)
+}
+
+function add (a, b) {
+    return Number(a) + Number(b)
+}
+
 describe("the string calculator", () => {
-    it("works", () => {
-        expect(1).toBe(1)
+    it ("sums two numbers separated by commas", () => {
+        expect(addNumbers("1, 2")).toBe(3)
+        expect(addNumbers("1, 2, 1")).toBe(4)
     })
 })
+
+// xit --> excluye el test indicado
+// fit / it.only --> ejecuta el test indicado
